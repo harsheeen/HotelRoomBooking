a5f64eed91711b7028b74be8452d511bd54699b7
harsheeen <harsheenkaur023@gmail.com>
2026-03-06 14:49:28 +0530

    Add: Room Search and Availability

--- RoomInventory/Main.java
+++ RoomInventory/Main.java
@@ -1,6 +1,22 @@
 package com.RoomInventory;
 import java.util.*;
+/*
+Project - HotelRoomBookingApp
+UseCase2 -   Room Search & Availability Check
+Key Concepts -
+*Read-only access
+*Defensive checks
+*Availability validation
 
+Key Requirements -
+*Display available room types
+*Show pricing and amenities
+*Prevent booking unavailable rooms
+
+@author HarsheenKaur
+@version 2.0
+
+*/
 public class Main {
 	static final Map<String, Integer> roomCount = new HashMap<>();
 	static final Map<String, Double> roomPrice = new HashMap<>();
@@ -11,22 +27,22 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
-
 		System.out.println("Welcome to Hotel Inventory System!");
 		ShowAvailability.showAvailability(roomCount);
 
+		//display options
 		while (true) {
 			System.out.print("\nEnter command (book/update/show/exit): ");
 			String command = sc.nextLine().trim().toLowerCase();
 
 			// Advanced switch with arrow syntax
 			switch (command) {
-			case "book" -> {
+			case "book" -> {		//book choice between suite, single, double
 				System.out.print("Enter room type (Suite/Single/Double): ");
 				String roomChoice = sc.nextLine().trim();
 				BookRooms.bookRoom(roomChoice,roomCount);
 			}
-			case "update" -> {
+			case "update" -> {		//update the price of certain rooms
 				System.out.print("Enter room type to update price: ");
 				String updateRoom = sc.nextLine().trim();
 				System.out.print("Enter new price: ");
@@ -34,7 +50,7 @@ public class Main {
 				sc.nextLine(); // consume newline
 				UpdatePrice.updatePrice(updateRoom, newPrice,roomPrice);
 			}
-			case "show" -> ShowAvailability.showAvailability(roomCount);
+			case "show" -> ShowAvailability.showAvailability(roomCount); 	//display the current status of the room
 			case "exit" -> {
 				System.out.println("Exiting system. Goodbye!");
 				sc.close();
